bb739783ac9d9b197b81279954e69e0f2e7e0f71
Jens Riisom Schultz <ibmurai@me.com>
2026-04-06 12:12:12 +0200

    Set sha256 for 0.1.0 release.

--- Formula/corklang.rb
+++ Formula/corklang.rb
@@ -2,7 +2,7 @@ class Corklang < Formula
   desc "Programming language that compiles to Commodore 64 machine code"
   homepage "https://github.com/corklang/cork"
   url "https://github.com/corklang/cork/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "dotnet"
